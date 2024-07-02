aacc3dd968e556a97a056731ec8b2ad94f4458ec
Luciano Daniel Escobar <lescobar-ext@aa2000.com.ar>
2024-07-01 23:47:26 -0300

    /

--- Sistema_David/Controllers/PedidosController.cs
+++ Sistema_David/Controllers/PedidosController.cs
@@ -43,6 +43,91 @@ namespace Sistema_David.Controllers
             return View();
         }
 
+        public ActionResult Listar(int idVendedor, DateTime FechaEntrega)
+        {
+            List<Pedido> result;
+
+            if (SessionHelper.GetUsuarioSesion() != null && SessionHelper.GetUsuarioSesion().IdRol == 2 || SessionHelper.GetUsuarioSesion().IdRol == 3) //ROL VENDEDOR
+            {
+                result = PedidosModel.ListaPedidos(SessionHelper.GetUsuarioSesion().Id, FechaEntrega);
+            }
+            else
+            {
+                result = PedidosModel.ListaPedidos(idVendedor, FechaEntrega);
+            }
+
+
+            return Json(new { data = result }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult ListarProductosPedido(int id)
+        {
+            try
+            {
+
+                var result = PedidosModel.ListaProductosPedido(id);
+                return Json(new { data = result }, JsonRequestBehavior.AllowGet);
+            }
+
+            catch (Exception ex)
+            {
+                return Json(null);
+            }
+
+        }
+
+        public ActionResult ListarEstados()
+        {
+            List<EstadosPedidos> usuarios;
+                usuarios = PedidosModel.ListaEstados();
+            return Json(new { data = usuarios }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult NuevoPedido(Pedidos pedido)
+        {
+            try
+            {
+
+
+                var result = PedidosModel.Nuevo(pedido);
+
+                return Json(new { Status = result });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Status = false });
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(int id)
+        {
+            try
+            {
+
+
+                //ANTES DE BORRAR LA VENTA, LE SUMAMOS EL STOCK AL USUARIO
+                var productosVenta = PedidosModel.ListaProductosPedido(id);
+                var venta = PedidosModel.BuscarPedido(id);
+
+                var resultProductosPedido = PedidosModel.EliminarProductos(id);
+                var result = PedidosModel.Eliminar(id);
+
+                if (result && resultProductosPedido)
+                    return Json(new { Status = true });
+
+                else
+                    return Json(new { Status = false });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Status = false });
+            }
+
+        }
+
     }
 
 }
