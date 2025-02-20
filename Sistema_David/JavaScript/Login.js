487b08b17647f6c866ef46eddb751b917c61e6f5
Luciano <luciano_5258@hotmail.com>
2025-02-19 23:56:38 -0300

    .

--- Sistema_David/JavaScript/Login.js
+++ Sistema_David/JavaScript/Login.js
@@ -1,63 +1,5 @@
-﻿///*var url =  "//Login/Login_User";*/
-
-//async function Loguear() {
-//    const $botonLogin = $('.boton-login'); // Selecciona el botón por clase
-
-//    // Mostrar la barra de carga
-//    $botonLogin.css('opacity', '1');
-
-//    // Cambiar el texto del botón a "Cargando..."
-//    $botonLogin.text('Cargando...');
-
-//    $('.datos-error').text('');
-//    $('.datos-error').addClass('d-none');
-
-//    try {
-//        let value = JSON.stringify({
-//            Usuario: document.getElementById("Usuario").value,
-//            Contrasena: document.getElementById("Contrasena").value
-//        });
-
-//        console.log('Enviando solicitud a /Login/Login_User');  // Agregar un log de envío
-
-//        let options = {
-//            type: "POST",
-//            url: '/Login/Login_User',
-//            async: true,
-//            data: value,
-//            contentType: "application/json",
-//            dataType: "json"
-//        };
-
-//        let result = await MakeAjax(options);
-
-//        if (result.Status) {
-//            $('.datos-error').text('');
-//            $('.datos-error').addClass('d-none');
-//            sessionStorage.setItem("usuario", JSON.stringify(result.Data));
-//            document.location.href = "../Ventas/Index";
-//        } else {
-//            $('.datos-error').text(result.Mensaje);
-//            $('.datos-error').removeClass('d-none');
-
-//            setTimeout(() => {
-//                $('.datos-error').text('');
-//                $('.datos-error').addClass('d-none');
-//            }, 2000);
-//        }
-//    } catch (error) {
-//        console.log('Error en la solicitud: ', error); // Mostrar más detalles del error
-//        $('.datos-error').text('Ha ocurrido un error: ' + error.message);
-//        $('.datos-error').removeClass('d-none');
-//    } finally {
-//        // Restaurar el texto del botón a "Conectarse"
-//        $botonLogin.text('Conectarse');
-//    }
-//}
-
-
+﻿
 $(document).ready(function () {
-
     // Verificar si el usuario tiene credenciales guardadas
     if (localStorage.getItem('rememberMe') === 'true') {
         // Si el checkbox estaba seleccionado la última vez
@@ -80,14 +22,12 @@ $(document).ready(function () {
         var data = {
             Usuario: username,
             Contrasena: password,
-            __RequestVerificationToken: token // Enviar el token CSRF
         };
 
         fetch(loginUrl, { // Aquí usamos la variable generada por Razor
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'RequestVerificationToken': token // Enviar el token CSRF
             },
             body: JSON.stringify(data)
         })
